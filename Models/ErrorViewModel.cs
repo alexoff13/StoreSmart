10ec6bfefc9937c55a7a4642fc2e515262852fda
alexoff13 <davydovaleksey932@gmail.com>
2020-10-11 13:10:36 +1000

    build project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EmployeeBase.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/SampleData.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeBase.Models
+{
+    public class SampleData
+    {
+        public static void Initialize(MobileContext context)
+        {
+            if (!context.Phones.Any())
+            {
+                context.Phones.AddRange(
+                    new Phone
+                    {
+                        Name = "Iphone X",
+                        Company = "Apple",
+                        Price = 600
+                    },
+                    new Phone
+                    {
+                        Name = "Samsung Galaxy S10",
+                        Company = "Samsung",
+                        Price = 550
+                    },
+                    new Phone
+                    {
+                        Name = "Pixel 3",
+                        Company = "Google",
+                        Price = 500
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
